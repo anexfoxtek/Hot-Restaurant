b2ea791ff8b52225480cdd52a17a313c7835fd48
unknown <anexfoxtek@gmail.com>
2019-07-13 12:52:50 -0400

    fixed table/reservations

--- server.js
+++ server.js
@@ -3,82 +3,50 @@ var path = require("path");
 var fs = require("fs");
 
 var app = express();
-
+var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 var tables = [
-   {
-       routeName:
-   }
 ];
 
 var awaiting = [
-   {
-
-   }
 ];
+
 app.get("/", function(req, res) {
-   res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.get("/tables", function(req, res) {
-   res.sendFile(path.join(__dirname, "tables.html"));
+app.get("/tables.html", function(req, res) {
+    res.sendFile(path.join(__dirname, "/tables.html"));
 });
 
-app.get("/reservations", function(req, res) {
-   res.sendFile(path.join(__dirname, "reservations.html"));
+app.get("/reservations.html", function(req, res) {
+    res.sendFile(path.join(__dirname, "/reservations.html"));
 });
 
-app.get("/api/tables/:table", function(req, res) {
-   var chosen = req.params.table;
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+});
 
-   console.log(chosen);
-
-   for (var i=0; i<tables.length; i++) {
-       if (chosen === tables[i].routeName) {
-           return res.json(tables[i]);
-       }
-   }
-   return res.json(false);
+app.get("/api/reservations", function(req, res) {
+    return res.json(awaiting);
 });
 
 app.post("/api/tables", function(req, res) {
-   var newTable = req.body;
-   newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
-
-   console.log(newTable);
-
-   tables.push(newTable);
-
-   res.json(newTable);
-});
-
-app.get("/api/waitinglist/:awaiting", function(req, res) {
-   var chosen = req.params.awaiting;
-
-   console.log(chosen);
-
-   for (var i=0; i<awaiting.length; i++) {
-       if (chosen === awaiting[i].routeName) {
-           return res.json(awaiting[i]);
-       }
-   }
-   return res.json(false);
-});
-
-app.post("api/waitinglist", function(req, res) {
-   var newAwaiting = req.body;
-   newAwaiting.routeName = newAwaiting.name.replace(/\s+/g, "").toLowerCase();
-
-   console.log(newAwaiting);
-
-   awaiting.push(newAwaiting);
-
-   res.json(newAwaiting);
+    var newTable = req.body;
+    
+    console.log(newTable);
+    if (tables.length <= 4) {
+    tables.push(newTable);
+    res.json(newTable);
+    } else {
+        awaiting.push(newTable);
+        res.json(newTable);
+    }    
 });
 
 app.listen(process.env.PORT || 3000, function() {
-   console.log("App listening");
+    console.log("App listening");
 });
